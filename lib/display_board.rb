8b011bc23c45e4ef6a147daeadc7a1b86cc4738d
Nyasia Richardson <rnyasia@yahoo.com>
2019-07-27 18:05:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,4 @@
-board =  [" ","x"," ","x"," ","o"," "," "," "]
-display board(board)
+board =  [" "," "," "," "," "," "," "," "," "]
 
 
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board

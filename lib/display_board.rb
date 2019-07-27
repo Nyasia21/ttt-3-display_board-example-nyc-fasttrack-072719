2e98d1b35e4cf311598d26bb91ca0aaa73ebec5a
Nyasia Richardson <rnyasia@yahoo.com>
2019-07-27 18:13:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@ board =  [" "," "," "," "," "," "," "," "," "]
 
 def display_board
   puts "   |   |   
-        -----------
-           |   |   
-        -----------
-           |   |   "
+-----------
+   |   |   
+-----------
+   |   |   "
 end 
 
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
